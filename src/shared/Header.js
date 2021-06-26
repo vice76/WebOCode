73db0d5fa154b73d8f7c8c6a432d65716df22adf
Sudheer Singh <sudheersid0710@gmail.com>
2021-06-26 19:44:53 +0530

    imported logo in header.js

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Logo from '../assets/Logo.png';
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <header>
+            <div className={navbarActiveclass.join(" ")}>
+                <div className="container">
+                    <div className="NavbarLeft">
+                        <Logo />
+                    </div>
+                </div>
+            </div>
+        </header>
     )
 }
 
