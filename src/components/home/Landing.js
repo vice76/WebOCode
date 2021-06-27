a60c19cc3c801f689c444b99983dd7ed085978a3
Sudheer Singh <sudheersid0710@gmail.com>
2021-06-27 11:06:47 +0530

    added dialog box functionality

--- src/components/home/Landing.js
+++ src/components/home/Landing.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Virus from '../../assets/virus.png';
+import Resourcedialog from '../../shared/Resourcedialog';
 
 const Landing = () => {
+    const [open , setOpen] = useState(false);
+    const openDialog = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
         <div className="landing-container">
             <div className="container">
@@ -10,13 +15,14 @@ const Landing = () => {
                    <p>Connect to help others</p>
                    <div className="button-container">
                        <button className="add-button">Add Resources</button>
-                       <button className="view-button">View Resources</button>
+                       <button className="view-button" onClick={openDialog}>View Resources</button>
                    </div>
                 </div>
                 <div className="container-right">
                     <img src={Virus} alt="virus"/>
                 </div>
             </div>
+            {open && <Resourcedialog open={open} handleClose={handleClose}/>}
         </div>
     )
 }
