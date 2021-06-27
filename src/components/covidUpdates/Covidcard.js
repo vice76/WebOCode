a094c5defc1fcb290c28313e32ceada1f14b6e5c
vice76 <arpityadavaj1999@gmail.com>
2021-06-27 20:06:45 +0530

    added dummy text to it

--- src/components/covidUpdates/Covidcard.js
+++ src/components/covidUpdates/Covidcard.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import "../../scss/components/_covidcard.scss";
 
 const Covidcard = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="covidcard">
+      <h3 className="place">xyz</h3>
+      <div className="covid-row">
+        <div className="covid-row__content-left">
+          <p>Active</p>
+          <h4 className="color-active">xyz</h4>
         </div>
-    )
-}
+        <div className="covid-row__content-right">
+          <p>Recovered</p>
+          <h4 className="color-recover">xyz</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Covidcard
+export default Covidcard;
