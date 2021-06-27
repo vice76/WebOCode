6553ea083ff578d3e0b97c74bbb947261bee32f8
Sudheer Singh <sudheersid0710@gmail.com>
2021-06-27 11:07:58 +0530

    added resources component

---
+++ src/components/resources/Oxygen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Oxygen = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Oxygen;
---
+++ src/components/resources/Plasma.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Plasma = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Plasma;
