6d4f4fd86edc5e311299b4f8afff1b5d75996255
vice76 <arpityadavaj1999@gmail.com>
2021-06-27 20:10:55 +0530

    made it covid card dynamic

--- src/components/covidUpdates/Covidcard.js
+++ src/components/covidUpdates/Covidcard.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "../../scss/components/_covidcard.scss";
 
-const Covidcard = () => {
+const Covidcard = ({data}) => {
   return (
     <div className="covidcard">
-      <h3 className="place">xyz</h3>
+      <h3 className="place">{data.region}</h3>
       <div className="covid-row">
         <div className="covid-row__content-left">
           <p>Active</p>
-          <h4 className="color-active">xyz</h4>
+          <h4 className="color-active">{data.activeCases}</h4>
         </div>
         <div className="covid-row__content-right">
           <p>Recovered</p>
-          <h4 className="color-recover">xyz</h4>
+          <h4 className="color-recover">{data.recovered}</h4>
         </div>
       </div>
     </div>
