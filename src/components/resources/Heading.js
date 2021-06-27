6040d06c3bc384ca9e1d2e691c77db6d9824edd5
Sudheer Singh <sudheersid0710@gmail.com>
2021-06-27 11:34:49 +0530

    done with heading

---
+++ src/components/resources/Heading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Heading = () => {
+    return (
+        <div className="heading-container">
+            <div className="container">
+                <h1>RESOURCES</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Heading
--- src/components/resources/Resources.js
+++ src/components/resources/Resources.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Heading from './Heading';
 
 const Resources = () => {
     return (
         <div>
-            resources
+            <Heading />
         </div>
     )
 }
