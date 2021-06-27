2f16ee4968dc2aa9dd3df618094e74ea4360a15f
Sudheer Singh <sudheersid0710@gmail.com>
2021-06-27 20:37:13 +0530

    removed bugs

--- src/components/home/Bloodbottle.js
+++ src/components/home/Bloodbottle.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Blood from "../../assets/Blood.png";
 import Bottle from "../../assets/Bottle.png";
 import Drop from "../../assets/Drop.png";
 import Donateicon from "../../assets/bx-donate-blood.png";
 import { Button } from '@material-ui/core';
 import "../../scss/components/_bloodbottle.scss";
+import ResourceForm from '../resources/ResourceForm'; 
 
 const Bloodbottle = () => {
+    const [openForm , setOpenForm] = useState(false);
+    const handleopenForm = () => setOpenForm(true);
+    const handleFormClose = () => setOpenForm(false);
+
     return (
         <div className="bloodbottle">
             <div className="bloodbottle-container">
@@ -15,12 +20,13 @@ const Bloodbottle = () => {
              <img className="bloodbottle-right" src={Drop} alt="background -img2"/> 
             </div>
              <div className="donate-button">
-             <Button variant="outlined" >Donate Plasma</Button>
+             <Button variant="outlined" onClick={handleopenForm}>Donate Plasma</Button>
              </div>
              <div className="plasma-heading">
                 <h1>BECOME A VOLUNTEER</h1> 
                 <img className="icon" src={Donateicon} alt="background -img2"/>  
              </div>
+             {openForm && <ResourceForm open={openForm} handleClose={handleFormClose}/>}
         </div>
     )
 }
