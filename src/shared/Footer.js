2d859d8525f0ef54df4b0c4ffcf420c4050dca0b
vice76 <arpityadavaj1999@gmail.com>
2021-06-27 17:24:10 +0530

    added love icon and break line

--- src/shared/Footer.js
+++ src/shared/Footer.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import Heart from "../assets/heart.png";
+import "../scss/components/_footer.scss";
 
-const Footer= () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer__row">
+        <p >
+          Made With Love <img src={Heart} alt="heart" style={{width:"15%"}}/>
+        </p>
+        <p>Contact Us</p>
+      </div>
+      <hr />
+      <div className="footer__center">
+        
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
