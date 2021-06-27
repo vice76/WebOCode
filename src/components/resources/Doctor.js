49b5feed45f29ecc8b4c4e8ebc5d1c75e42b630b
Sudheer Singh <sudheersid0710@gmail.com>
2021-06-27 18:26:37 +0530

    added content for doctor component

--- src/components/resources/Doctor.js
+++ src/components/resources/Doctor.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Doctor from '../../assets/Doctor.png';
 
 const Doctor = () => {
     return (
         <div className="Doctor-div">
-            
+            <div className="container">
+                <div className="container-left">
+                    <img src={Doctor} alt="doctor-png" />
+                </div>
+                <div ClassName="container-right">
+                    <p>“We can change the world and make it a better place. It is in our hands to make a difference.” Nelson Mandela</p>
+                </div>
+            </div>
         </div>
     )
 }
