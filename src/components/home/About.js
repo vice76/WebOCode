6b7d7028651a993db542f961cc7d7b2c1f8a5ea5
Sudheer Singh <sudheersid0710@gmail.com>
2021-06-27 20:29:41 +0530

    did footer

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -1,25 +1,35 @@
 import React from 'react'
-import AboutLeft from '../../assets/aboutleft.png';
-import LogoIcon from '../../assets/LogoIcon.png';
-import Reactangleabout from "../../assets/Rectangle-about.png";
+import LogoIC from '../../assets/LogoIC.png';
+import HelpingHand from '../../assets/helpingHand.png';
 
 const About=()=> {
     return (
+        <div className="about-div">
+        <div className="container-one">
         <div className="about-left">
-            <img  src={AboutLeft} alt="about-left" />
-            <div className="question">
-                <h3>What Is Sanjeevni ?</h3>
-            </div>
-            <img className="about-right" src={Reactangleabout} alt="about-info" style={{position: "relative"}}/>
-            <div className="aboutsanjeevni">
+            <img src={LogoIC} className="about-logo" alt="website-logo"/>
+        </div>
+        <div className="aboutsanjeevni">
                 <h3>Sanjeevni is a initiative to help people by providing
                      them a platform to find the availability of medicines, injections , 
                     plasma and oxygen in their city. 
-                    We also request people if they have any valid resource
+                </h3>
+        </div>
+        </div>
+
+        <div className="container-one">
+            <div className="aboutsanjeevni2">
+                <h3>
+                We also request people if they have any valid resource
                      of medical needs mentioned above can put them up.
                       Your one help can save millions life.
-                    Stay safe , Stay healthy</h3>
+                    Stay safe , Stay healthy
+                </h3>
             </div>
+            <div className="about-right">
+                <img src={HelpingHand} alt="helpingHandLogo" />
+            </div>
+        </div>
         </div>
     )
 }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Landing from './Landing';
 import Bloodbottle from './Bloodbottle';
+import About from './About';
 
 const Home = () => {
     return (
         <div>
             <Landing />
+            <About />
             <Bloodbottle />
         </div>
     )
